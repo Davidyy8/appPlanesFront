0830ca861379e4252c3aec3f1937cdaa7023ea7b
Davidyy8 <davidolayarodriguez@gmail.com>
2026-04-22 11:48:08 +0200

    Delete appPlanesFront directory

--- appPlanesFront/src/app/components/upload-plan/upload-plan.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-upload-plan',
-  imports: [],
-  templateUrl: './upload-plan.html',
-  styleUrl: './upload-plan.css',
-})
-export class UploadPlan {}
